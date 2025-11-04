2801f3206036c4b479eab8ec895f084af1299244
Uhrbaan <uhrbaan@gmx.net>
2025-11-04 13:53:56 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"hyperdrive/remote/hyperdrive"
+	"log"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/uuid"
+)
+
+const (
+	rpiIp    = "10.42.0.1"
+	mqttPort = ":1883"
+	// docsPort = ":18443"
+)
+
+func main() {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(rpiIp + mqttPort)
+	opts.SetClientID(uuid.NewString())
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal("Could not establish connection with MQTT server: ", token.Error())
+	}
+
+	host := hyperdrive.DefaultHost()
+	topic := host.StatusIntendedDiscoverSubscriptionTopic()
+	content := hyperdrive.DiscoverSubscriptionIntentPayload{
+		Topic: 
+	}
+}
